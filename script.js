af299184d0d7744c04d1da9a32b398d483cce357
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2025-08-22 20:45:36 -0400

    Begin calculator project Create HTML skeleton and basic CSS Create very rough first draft of project

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(a, b){
+    const sum = a + b;
+    return sum;
+}
+
+function subtract(a, b){
+    const difference = a - b;
+    return difference;
+}
+
+function multiply(a, b){
+    const mulitple = a * b;
+    return mulitple;
+}
+
+function divide(a, b){
+    const division = a / b;
+    return division;
+}
