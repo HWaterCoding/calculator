f527d683054917ef3232208f37daf7a6a5fe30e3
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2025-08-26 17:37:18 -0400

    Add event listeners forEach button on calc Create exceptions for appending values to display

--- script.js
+++ script.js
@@ -1,19 +1,87 @@
-function add(a, b){
-    const sum = a + b;
-    return sum;
+
+//store calc display in display variable
+//add event listener to every button on page, when clicked store value of button in display
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => {
+    button.addEventListener('click', () =>{
+        const value = button.value;
+        switch(button.value){
+        case "clear":
+            display.value = "";
+            break;
+        case "posNeg":
+            break;
+        case "percent":
+            display.value = parseFloat(display.value) / 100;
+            break;
+        case "divide":
+            break;
+        case "multiply":
+            break;
+        case "subtract":
+            break;
+        case "add":
+            break;
+        case "equal":
+            break;
+        case "backspace":
+            break;
+        default: 
+            display.value += value;
+        } 
+    })
+});
+
+
+
+
+
+
+// const operators = [];
+
+
+
+
+
+
+
+//working
+const addition = (a, b) => a + b;
+const subtraction = (a, b) => a - b;
+const multiplication = (a, b) => a * b;
+const division = (a, b) => a / b;
+
+//the parameter will be the current display.value
+function posNeg(){
+//for the +/- button, create a conditional that states if the current number is positive, change it to negative
+//and if the current value is negative, change it to positive.
 }
 
-function subtract(a, b){
-    const difference = a - b;
-    return difference;
+
+
+
+
+let num1
+let operator
+let num2
+
+function operate(operator, num1, num2){
+
 }
 
-function multiply(a, b){
-    const mulitple = a * b;
-    return mulitple;
-}
 
-function divide(a, b){
-    const division = a / b;
-    return division;
-}
+
+
+
+
+
+
+//add event listeners to each integer that APPEND that value to the display
+//store the current value of the display in a seperate variable for later use
+
+
+
+
+
+
