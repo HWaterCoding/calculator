942150ae0c52023944cbdea26c0a291b9423d760
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2025-09-01 16:59:40 -0400

    Write initial pseudocode ideas for how to solve

--- script.js
+++ script.js
@@ -11,75 +11,59 @@ buttons.forEach(button => {
             display.value = "";
             break;
         case "posNeg":
+            display.value = display.value * -1;
             break;
         case "percent":
             display.value = parseFloat(display.value) / 100;
             break;
-        case "divide":
-            break;
-        case "multiply":
-            break;
-        case "subtract":
-            break;
-        case "add":
-            break;
         case "equal":
+            //run operate function on display
             break;
         case "backspace":
+            display.value = display.value.slice(0, -1);
             break;
         default: 
             display.value += value;
+            display.scrollLeft = display.scrollWidth;
         } 
     })
 });
 
 
+// let num1 = ;
+// let operator = ;
+// let num2 = ;
 
 
+const operators = ["+", "-", "÷", "x"];
+
+function operate(num1, operator, num2){
+    
+}
+
+//add every operator to an array
+//iterate through that array to see if there's an operator in the display
+//if there is, split the display at the operator
+//left side = num1, right side = num2, operator = operator
+//create conidtional statement... ex... if(operator = "+"){addition()}
+//update the display.value with the result of the operation
 
 
 // const operators = [];
 
 
+//EQUALS LOGIC?
+//break every character in the display down into an array
+//use the includes() method to test which operator is included in the display
+//use an if() statement to determine which of the below operations to perform on the number
 
 
 
-
-
-//working
+//working functions for basic operation
 const addition = (a, b) => a + b;
 const subtraction = (a, b) => a - b;
 const multiplication = (a, b) => a * b;
 const division = (a, b) => a / b;
-
-//the parameter will be the current display.value
-function posNeg(){
-//for the +/- button, create a conditional that states if the current number is positive, change it to negative
-//and if the current value is negative, change it to positive.
-}
-
-
-
-
-
-let num1
-let operator
-let num2
-
-function operate(operator, num1, num2){
-
-}
-
-
-
-
-
-
-
-
-//add event listeners to each integer that APPEND that value to the display
-//store the current value of the display in a seperate variable for later use
-
 
 
 
