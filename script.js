551ecd9cff39f0e9b60facd16e43b12375e0f41c
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2025-09-02 17:51:33 -0400

    Finish operate function logic Adjust "equal" and "clear" logic properly

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+// initialize variables for calculator logic
+let num1
+let operator
+let num2
 
-//store calc display in display variable
-//add event listener to every button on page, when clicked store value of button in display
+
+//add onclick listener to every button on calc to append value in display
+//use switch statement to create exceptions for buttons to not be appended to display
 const display = document.getElementById("display");
 const buttons = document.querySelectorAll("button");
 buttons.forEach(button => {
@@ -9,6 +14,9 @@ buttons.forEach(button => {
         switch(button.value){
         case "clear":
             display.value = "";
+            num1 = "";
+            num2 = "";
+            operator = "";
             break;
         case "posNeg":
             display.value = display.value * -1;
@@ -16,11 +24,22 @@ buttons.forEach(button => {
         case "percent":
             display.value = parseFloat(display.value) / 100;
             break;
-        case "equal":
-            //run operate function on display
-            break;
         case "backspace":
             display.value = display.value.slice(0, -1);
+            break;
+        case "+":
+        case "-":
+        case "x":
+        case "÷":
+            num1 = parseFloat(display.value);
+            operator = button.value;
+            display.value = "";
+            //the above works, but let's come back and fix it so that the display is cleared when 
+            //a new number is entered, NOT when the operator is pressed.
+            break;
+        case "equal":
+            num2 = display.value;
+            display.value = operate(num1, operator, num2);
             break;
         default: 
             display.value += value;
@@ -29,41 +48,37 @@ buttons.forEach(button => {
     })
 });
 
+//calculator works but needs to be improved. Things to improve:
+//when a second operator is clicked, we need to execute the operate function or "equal" case so that it works 
+//like a regular calculator and updates the display value. otherwise 3x3x3 = 9 instead of 27.
 
-// let num1 = ;
-// let operator = ;
-// let num2 = ;
 
 
-const operators = ["+", "-", "÷", "x"];
 
+//basic functions for calculator logic to operate on numbers
 function operate(num1, operator, num2){
-    
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    switch(operator){
+        case "+":
+            return num1 + num2;
+        case "-":
+            return num1 - num2;
+        case "x":
+            return num1 * num2;
+        case "÷":
+            return num1 / num2;
+    }
 }
 
-//add every operator to an array
-//iterate through that array to see if there's an operator in the display
-//if there is, split the display at the operator
-//left side = num1, right side = num2, operator = operator
-//create conidtional statement... ex... if(operator = "+"){addition()}
-//update the display.value with the result of the operation
 
-
-// const operators = [];
-
-
-//EQUALS LOGIC?
-//break every character in the display down into an array
-//use the includes() method to test which operator is included in the display
-//use an if() statement to determine which of the below operations to perform on the number
-
-
-
-//working functions for basic operation
 const addition = (a, b) => a + b;
 const subtraction = (a, b) => a - b;
 const multiplication = (a, b) => a * b;
 const division = (a, b) => a / b;
+
+
+
 
 
 
